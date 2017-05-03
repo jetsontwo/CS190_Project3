160118192f364ad9a1ac38c2736e22afe7f34f45
jetsontwo <jetsontwo@gmail.com>
2017-05-02 17:31:52 -0700

    Players can Pick Up Collectables
    Players can now pick up items that they walk over.

---
+++ Assets/Scripts/Player_Collect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Collect : MonoBehaviour {
+
+    private GameObject object_held;
+
+	void OnTriggerEnter(Collider c)
+    {
+        if(!object_held)
+        {
+            object_held = c.gameObject;
+            c.GetComponent<BoxCollider>().enabled = false;
+            c.transform.parent = gameObject.transform;
+            c.transform.localPosition = new Vector3(0.75f, 0, 0.75f);
+            c.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
