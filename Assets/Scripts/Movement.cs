4e2872692770eb2376f932d3c88a09f89af57c0e
jetsontwo <jetsontwo@gmail.com>
2017-05-02 17:09:32 -0700

    Fixed 3D Movement
    Player moves forward based on local axis and can strafe.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,17 +16,15 @@ public class Movement : MonoBehaviour {
 	void Update () {
         vert = Input.GetAxisRaw("Vertical");
         horiz = Input.GetAxisRaw("Horizontal");
-        
-        if (vert != 0 && rb.velocity.magnitude < max_vel)
-            rb.velocity += new Vector3(0, 0, vert);
-        else if (vert == 0)
-            rb.velocity -= new Vector3(0, 0, rb.velocity.z / decelleration);
-        if (horiz != 0 && rb.velocity.magnitude < max_vel)
-            rb.velocity += new Vector3(horiz, 0, 0);
-        else if (horiz == 0)
-            rb.velocity -= new Vector3(rb.velocity.x / decelleration, 0, 0);
 
-        if(vert != 0 || horiz != 0)
+
+        if (vert != 0 && rb.velocity.magnitude < max_vel)
+            rb.velocity += transform.forward * vert;
+        if (horiz != 0 && rb.velocity.magnitude < max_vel)
+            rb.velocity += transform.right * horiz;
+
+
+        if (vert != 0 || horiz != 0)
         {
             rb.velocity.Normalize();
             rb.velocity *= Time.deltaTime * speed;
