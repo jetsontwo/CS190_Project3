ec341cbbea604d54b8abfa8c6110820b66902cdb
jetsontwo <jetsontwo@gmail.com>
2017-05-09 17:12:05 -0700

    Players Only Pickup When Pressing Space Now
    Now the player detects when it collides with an object and then picks it
    up only when space is pressed. Will later add a drop feature.

--- Assets/Scripts/Player_Collect.cs
+++ Assets/Scripts/Player_Collect.cs
@@ -6,15 +6,30 @@ public class Player_Collect : MonoBehaviour {
 
     private GameObject object_held;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            pickup();
+    }
+
+    void pickup()
+    {
+        object_held.GetComponent<BoxCollider>().enabled = false;
+        object_held.transform.parent = gameObject.transform;
+        object_held.transform.localPosition = new Vector3(0.75f, 0, 0.75f);
+        object_held.transform.localRotation = Quaternion.identity;
+    }
 	void OnTriggerEnter(Collider c)
     {
         if(!object_held)
         {
             object_held = c.gameObject;
-            c.GetComponent<BoxCollider>().enabled = false;
-            c.transform.parent = gameObject.transform;
-            c.transform.localPosition = new Vector3(0.75f, 0, 0.75f);
-            c.transform.localRotation = Quaternion.identity;
+            
         }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject == object_held)
+            object_held = null;
     }
 }
