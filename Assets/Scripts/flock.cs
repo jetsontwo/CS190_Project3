6982b6f419167e05989028f734f2bf69f746311d
kagekyaa <kevin_hokagero@yahoo.com>
2017-05-29 18:43:54 -0700

    added flocking manager, fish prefab, animal testing scene
    -fish prefab currently use sphere gameobject, will update later
    -have not yet placed in world scene

---
+++ Assets/Scripts/flock.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flock : MonoBehaviour {
+
+    public float speed = 3.0f;
+    public float rotation_speed = 4.0f;
+    Vector3 average_Heading;
+    Vector3 average_Position;
+    public float neighbor_distance = 1.0f;
+
+    bool turning = false;
+
+	// Use this for initialization
+	void Start () {
+        speed = Random.Range(0.5f, 1);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Vector3.Distance(transform.position,global_flock.Position.position)>= global_flock.world_size_x)
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+
+        }
+
+        if(turning)
+        {
+            Vector3 direction = global_flock.Position.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotation_speed * Time.deltaTime);
+            speed = Random.Range(0.5f, 1);
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1)
+            {
+                flocking_rules();
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+	}
+    void flocking_rules()
+    {
+        GameObject[] group_pos;
+        group_pos = global_flock.mygroup;
+
+        Vector3 vcentre = global_flock.Position.position;
+        Vector3 vavoid = Vector3.zero;
+
+        float group_speed = 0.1f;
+
+        Vector3 final_position = global_flock.final_position;
+        float distance;
+        int group_size = 0;
+
+        foreach (GameObject mygo in group_pos)
+        {
+            if(mygo != this.gameObject)
+            {
+                distance = Vector3.Distance(mygo.transform.position, this.transform.position);
+                if(distance <= neighbor_distance)
+                {
+                    vcentre += mygo.transform.position;
+                    group_size++;
+
+                    if(distance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - mygo.transform.position);
+                    }
+
+                    flock another_object = mygo.GetComponent<flock>();
+                    group_speed = group_speed + another_object.speed;
+
+                }
+            }
+        }
+
+        if (group_size > 0)
+        {
+            vcentre = vcentre / group_size + (final_position - this.transform.position);
+            speed = group_speed / group_size;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+
+            if(direction != global_flock.Position.position)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotation_speed * Time.deltaTime);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/global_flock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class global_flock : MonoBehaviour {
+
+    public GameObject myprefab;
+    public GameObject goalprefab;
+
+    public Transform original_position;
+
+    public static Transform Position;
+
+    public static int world_size_x = 10;
+    public static int world_size_y = 2; 
+    public static int world_size_z = 10;
+
+    public static int num_of_objects = 20;
+
+    public static GameObject[] mygroup = new GameObject[num_of_objects];
+
+    public static Vector3 final_position = Vector3.zero;
+	
+    void awake()
+    {
+        Position = transform;
+    }
+
+    // Use this for initialization
+	void Start () {
+        Position = transform;
+
+        for (int i = 0; i < num_of_objects; i++)
+        {
+            Vector3 position = new Vector3(original_position.position.x + Random.Range(-world_size_x, world_size_x),
+                                    original_position.position.y + Random.Range(-world_size_y, world_size_y),
+                                    original_position.position.z + Random.Range(-world_size_z, world_size_z));
+            mygroup[i] = (GameObject)Instantiate(myprefab, position, Quaternion.identity);
+        }
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Random.Range(0,1000) < 50)
+        {
+            final_position = new Vector3(original_position.position.x + Random.Range(-world_size_x, world_size_x),
+                                    original_position.position.y + Random.Range(-world_size_y, world_size_y),
+                                    original_position.position.z + Random.Range(-world_size_z, world_size_z));
+            //goalprefab.transform.position = final_position;
+        }
+	}
+}
