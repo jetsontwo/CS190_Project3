1e9018b987f72cf7ea458dacfe51aab3d1909ea3
jetsontwo <jetsontwo@gmail.com>
2017-05-30 15:54:01 -0700

    Added Activate Sound Scipt to Turn on GameObjects with Sound
    Will be placed on each item as it is placed on the post it will activate
    this script.  It can be given a time so that they don't all turn on at
    once as well.

---
+++ Assets/Scripts/Activate_Sounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate_Sounds : MonoBehaviour {
+
+
+    public GameObject[] list_of_objects;
+
+    public IEnumerator activate(float time_between = 0)
+    {
+        foreach (GameObject item in list_of_objects)
+        {
+            item.SetActive(true);
+            yield return new WaitForSeconds(time_between);
+        }
+    }
+}
