0cd5b3ffc003713669829099e9528129782c02ba
jetsontwo <jetsontwo@gmail.com>
2017-06-01 14:55:32 -0700

    Added Wind Sound Effects
    Remade the SoundBank

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ENV_BIRD_CHIRPING1 = 326967116U;
+        static const AkUniqueID ENV_BIRD_CHIRPING2 = 326967119U;
+        static const AkUniqueID PLAYERWALK_CONCRETE = 1685368833U;
+        static const AkUniqueID PLAYERWALK_DIRT = 3277136171U;
+        static const AkUniqueID PLAYERWALK_GRAVEL = 1592501941U;
+        static const AkUniqueID PLAYERWALK_TILE = 2308746324U;
+        static const AkUniqueID PLAYERWALK_WATER = 1876925021U;
+        static const AkUniqueID PLAYERWALK_WOOD = 3420637487U;
+        static const AkUniqueID WIND = 1537061107U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
