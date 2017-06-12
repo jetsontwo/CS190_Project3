e9e1c19d89ab0da9fe1ddc63ae8168370cc20378
kagekyaa <kevin_hokagero@yahoo.com>
2017-06-12 16:44:46 -0700

    added water animation

--- Assets/Scripts/Activate_Sounds.cs
+++ Assets/Scripts/Activate_Sounds.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Activate_Sounds : MonoBehaviour {
 
     public GameObject[] list_of_objects;
+    public float time_between = 0f;
 
-    public IEnumerator activate(float time_between = 0)
+    public IEnumerator activate(float time_between_var = 0f)
     {
+        if(time_between <= time_between_var)
+        {
+            time_between = time_between_var;
+        }
+
+        this.GetComponent<event_trigger>().enabled = true;
+
         foreach (GameObject item in list_of_objects)
         {
             item.SetActive(true);
             yield return new WaitForSeconds(time_between);
         }
-        this.GetComponent<event_trigger>().enabled = true;
 
 
 
