32238e7b4be7154aee7b8a3843347d8ed0c742d5
kagekyaa <kevin_hokagero@yahoo.com>
2017-06-13 09:11:59 -0700

    squirrel, new hint sounds, fish
    -lowered flapping fish volume
    -added 5 new hint sounds to the world
    -added squirrel sounds

--- CS190_Project3_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
+++ CS190_Project3_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,20 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BUTTONPRESS = 317641954U;
         static const AkUniqueID ENV_BIRD_CHIRPING1 = 326967116U;
         static const AkUniqueID ENV_BIRD_CHIRPING2 = 326967119U;
         static const AkUniqueID FISH_SOUNDS = 2839789934U;
+        static const AkUniqueID HINT1_PLAY = 1792468376U;
+        static const AkUniqueID HINT1_STOP = 4123339242U;
+        static const AkUniqueID HINT2_PLAY = 656448445U;
+        static const AkUniqueID HINT2_STOP = 2883764011U;
+        static const AkUniqueID HINT3_PLAY = 209538446U;
+        static const AkUniqueID HINT3_STOP = 1211077744U;
+        static const AkUniqueID HINT4_PLAY = 889616867U;
+        static const AkUniqueID HINT4_STOP = 3246161321U;
+        static const AkUniqueID HINT5_PLAY = 4068216044U;
+        static const AkUniqueID HINT5_STOP = 1743033334U;
         static const AkUniqueID HINTS = 3420417565U;
         static const AkUniqueID PICK_UP_OBJECT = 3861027660U;
         static const AkUniqueID PICKUP = 3978245845U;
@@ -28,6 +39,7 @@ namespace AK
         static const AkUniqueID PLAYERWALK_WATER = 1876925021U;
         static const AkUniqueID PLAYERWALK_WATER_CLOSE = 3668604724U;
         static const AkUniqueID PLAYERWALK_WOOD = 3420637487U;
+        static const AkUniqueID SQUIRREL_PLAY = 3260844493U;
         static const AkUniqueID WAVES = 2453122309U;
         static const AkUniqueID WIND = 1537061107U;
     } // namespace EVENTS
